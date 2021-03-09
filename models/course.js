74773664d3b787b8d57ee882cc9c209e1e41d3df
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-03-09 18:39:27 +0100

    last

--- models/course.js
+++ models/course.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
 const courseSchema = new mongoose.Schema({
-    title : String,
-    author: String,
-    tags : [String],
-    price : Number,
+    title : {type :String, required : true},
+    author: { id : {type : mongoose.Schema.Types.ObjectId, ref :'Author'}, name :String},
+    tags : {type :[String], validate : { validator : function (v) { return v.length > 0} , 
+                                        message: 'Course tags must have one tag at least'}},
+    price : {type : Number, required : function() { return this.isPublished == true}},
     date : {type : Date, default : Date.now()},
     isPublished : Boolean
 });
--- routers/courses.js
+++ routers/courses.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const _ = require('lodash');
+const Author = require('../models/author');
 const Course = require('../models/course');
 
 router.get('', async (req,res)=>{
@@ -7,7 +9,41 @@ router.get('', async (req,res)=>{
 })
 
 router.get('/:id', async (req,res)=>{
+    let course = await Course.findById(req.params.id)
+                                .populate('author.id')
+    if(!course)
+        return res.status(404).send('Course Id is not found')
+    res.send(course)
+})
+
+router.post('/', async (req,res)=>{
+    let author = await Author.findById(req.body.author.id);
+    if(!author)
+        return res.status(404).send('Author Id is not found')
+    req.body.author.name=author.name
+    let course = await new Course(_.pick(req.body, ['title','author','tags','price','isPublished']));
+    try {
+        course = await course.save();
+    } catch (error) {
+        return res.status(400).send("Error Store in DB : "+error.message)
+    }
+
+    res.status(201).send(course)
+})
+
+router.put('/:id', async (req,res)=>{
     let course = await Course.findById(req.params.id);
+    if(!course)
+        return res.status(404).send('Course Id is not found')
+    /* if(req.body.title)
+        course.title = req.body.title */
+    course = _.merge(course,req.body);
+    course = await course.save();
+    res.send(course)
+})
+
+router.delete('/:id', async (req,res)=>{
+    let course = await Course.findByIdAndDelete(req.params.id);
     if(!course)
         return res.status(404).send('Course Id is not found')
     res.send(course)
